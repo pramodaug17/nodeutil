862d3cbd73c7844566d9cc3229ef12f6ff6a8a29
Pramod Jain <pramod.aug17@gmail.com>
2020-04-07 00:21:55 +0530

    removing dist files

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var session_passport = require("./session-passport");
-module.exports = {
-    session_passport: session_passport
-};
