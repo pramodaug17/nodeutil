e818f2a23f9df4642fae33e2d678940297f402a8
Pramod Jain <pramod.aug17@gmail.com>
2020-04-07 00:36:44 +0530

    1.1.4

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var session_passport = require("./session-passport");
+module.exports = {
+    session_passport: session_passport
+};
